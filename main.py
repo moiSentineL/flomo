30bcc085e0d8d3936648b8dbd4b7e53a175411f5
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-06-02 10:52:33 +0530

    added basic repo configs!

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 start = datetime.now()
 while True:
-	now = datetime.now()
-	print("\r"+str(now-start), end="")
+    now = datetime.now()
+    print("\r"+str(now-start), end="")
 
-#Source: https://gist.github.com/dzamlo/9debd3284750f3eed2a4654f0ec908e8
+# Source: https://gist.github.com/dzamlo/9debd3284750f3eed2a4654f0ec908e8
