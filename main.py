affbdeee1d740b15d76baee3ed5f90453fce9a4a
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-06-05 13:08:00 +0530

    modified it to match our needs

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 #! /bin/env python3
 import time
 
-print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
-input()
-print('Started.')
 start_time = time.time()
-last_time = start_time
-lap_num = 1
+print("Started flow!")
 
-try:
-    while True:
-        input()
-        lap_time = round(time.time() - last_time, 2)
-        total_time = round(time.time() - start_time, 2)
-        print(f'Lap #{lap_num}: {total_time} ({lap_time})', end='')
-        lap_num += 1
-        last_time = time.time()
-except KeyboardInterrupt:
-    print('\nDone.')
+while True:
+    input()
+    time_elapsed = round(time.time() - start_time, 2)
+    print("Time elapsed: ", time_elapsed)
+
+    time_for_break = time_elapsed/5
+    print("Time for break: ", time_for_break)
+
+    break
