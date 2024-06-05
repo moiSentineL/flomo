de5fff466019686bedbbf466aae1dd2ebbae5634
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-06-05 12:56:37 +0530

    a working stopwatch code

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 #! /bin/env python3
-from datetime import datetime
-start = datetime.now()
-while True:
-    now = datetime.now()
-    print("\r"+str(now-start), end="")
+import time
 
-# Source: https://gist.github.com/dzamlo/9debd3284750f3eed2a4654f0ec908e8
+print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
+input()
+print('Started.')
+start_time = time.time()
+last_time = start_time
+lap_num = 1
+
+try:
+    while True:
+        input()
+        lap_time = round(time.time() - last_time, 2)
+        total_time = round(time.time() - start_time, 2)
+        print(f'Lap #{lap_num}: {total_time} ({lap_time})', end='')
+        lap_num += 1
+        last_time = time.time()
+except KeyboardInterrupt:
+    print('\nDone.')
