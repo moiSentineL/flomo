7f872eed1e0d975f3539df40882fcb62285a3d7e
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-06-06 17:12:17 +0530

    commands.py -> main.py

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from src.logic import Flow
+import click
+# from src.logic import Flow
 
-flow = Flow()
-flow.initialize_flow()
-print("Started flow!")
+# flow = Flow()
 
-print("Waiting for input..")
-input()
+# flow.initialize_flow()
+# print("Started flow!")
 
-flow.break_flow()
-print("Time elapsed: ", flow.time_elapsed, "s")
-print("Time for break: ", flow.time_for_break, "s")
+# print("Waiting for input..")
+# input()
 
-# Wait for flow.time_for_break
-flow.restart_flow()
-print("Restarted Flow!")
+# flow.break_flow()
+# print("Time elapsed: ", flow.time_elapsed, "s")
+# print("Time for break: ", flow.time_for_break, "s")
 
-# repeat cycle until stop_flow
+# # Wait for flow.time_for_break
+# flow.restart_flow()
+# print("Restarted Flow!")
+
+# # repeat cycle until stop_flow
+
+
+@click.group()
+def hello():
+    click.echo("host group")
+
+
+@hello.command()
+@click.option('-t', '--tag', default='Default', help='Session tag name.')
+@click.option('-n', '--name', default='Work',
+              help='Session Name')
+def start(tag, name):
+    click.echo("started")
+    click.echo(f"tag: {tag}")
+    click.echo(f"name: {name}")
+
+
+if __name__ == '__main__':
+    hello()
