d1d0968b395ab7f370448658c14c2279ab386a78
moiSentineL <nibir.patowary@gmail.com>
2024-06-06 16:26:22 +0530

    moved static.py

--- src/static.py
+++ src/static.py
@@ -1,75 +1,56 @@
 import shutil
 import os
-import platform
-import signal
 import time
 import threading
 
 first_name = " First Name: "
 tag = " Tag: "
-stopwatch = 0
-
+stopwatch = 0  
 
 def format_time(seconds):
     mins, secs = divmod(seconds, 60)
     return f"{mins:02}:{secs:02}"
 
-
-def print_empty_square(size, first_name="", tag="", stopwatch=""):
-    terminal_size = shutil.get_terminal_size()
-    vet = terminal_size.columns
-    hai = terminal_size.lines
-
-    left_padding = (vet - (size * 2 + 2)) // 2
-    top_padding = (hai - (size + 2)) // 2
-
+def print_empty_rectangle(width, height, first_name="", tag="", stopwatch=""):
+    left_padding = (width - 30) // 2
+    top_padding = (height - 7) // 2
+    
     print("\n" * top_padding, end="")
-    print(" " * left_padding + "----FLOMO-----------------------")
-
-    for i in range(size):
+    print(" " * left_padding + "+" + "-" * 28 + "+")
+    
+    for i in range(5):
         if i == 1 and first_name:
-            print(" " * left_padding + "8" + first_name.ljust(size * 2) + "8")
+            print(" " * left_padding + "|" + first_name.center(28) + "|")
         elif i == 2 and tag:
-            print(" " * left_padding + "8" + tag.ljust(size * 2) + "8")
+            print(" " * left_padding + "|" + tag.center(28) + "|")
         elif i == 3 and stopwatch:
-            print(" " * left_padding + "8" + stopwatch.ljust(size * 2) + "8")
+            print(" " * left_padding + "|" + stopwatch.center(28) + "|")
         else:
-            print(" " * left_padding + "8" + " " * (size * 2) + "8")
-
-    print(" " * left_padding + "8" + "_" * (size * 2) + "8")
-
+            print(" " * left_padding + "|" + " " * 28 + "|")
+    
+    print(" " * left_padding + "+" + "-" * 28 + "+")
 
 def clear_terminal():
-    if platform.system() == "Linux" or platform.system() == "Darwin":
-        os.system("clear")
-    elif platform.system() == "Windows":
-        os.system("cls")
-
-
-def handle_resize(signum, frame):
-    clear_terminal()
-    print_empty_square(size, first_name, tag, format_time(stopwatch))
-
+    os.system("cls" if os.name == "nt" else "clear")
 
 def update_stopwatch():
     global stopwatch
     while True:
         time.sleep(1)
         stopwatch += 1
-        handle_resize(None, None)
 
+def get_terminal_size():
+    columns, lines = shutil.get_terminal_size()
+    return columns, lines
 
-if __name__ == "__main__":
-    size = 15
-
-    signal.signal(signal.SIGWINCH, handle_resize)
-
-    clear_terminal()
-    print_empty_square(size, first_name, tag, format_time(stopwatch))
+def main():
     threading.Thread(target=update_stopwatch, daemon=True).start()
 
     while True:
-        try:
-            time.sleep(1)
-        except KeyboardInterrupt:
-            break
+        clear_terminal()
+        width, height = get_terminal_size()
+        print_empty_rectangle(width, height, first_name, tag, format_time(stopwatch))
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
