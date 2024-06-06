d4500312907edf6ecfe34ede6111e2e5ebc57cbf
moiSentineL <nibir.patowary@gmail.com>
2024-06-06 16:09:05 +0530

    signal -> threading...?

--- static.py
+++ static.py
@@ -1,9 +1,8 @@
 import shutil
 import os
-import platform
-import signal
 import time
 import threading
+import curses
 
 first_name = " First Name: "
 tag = " Tag: "
@@ -13,56 +12,48 @@ def format_time(seconds):
     mins, secs = divmod(seconds, 60)
     return f"{mins:02}:{secs:02}"
 
-def print_empty_square(size, first_name="", tag="", stopwatch=""):
+def print_empty_rectangle(width, height, first_name="", tag="", stopwatch=""):
     terminal_size = shutil.get_terminal_size()
-    vet = terminal_size.columns
-    hai = terminal_size.lines
-    
-    left_padding = (vet - (size * 2 + 2)) // 2
-    top_padding = (hai - (size + 2)) // 2
+    left_padding = (terminal_size.columns - width) // 2
+    top_padding = (terminal_size.lines - height) // 2
     
     print("\n" * top_padding, end="")
-    print(" " * left_padding + "----FLOMO-----------------------")
+    print(" " * left_padding + "+" + "-" * (width - 2) + "+")
     
-    for i in range(size):
+    for i in range(height - 2):
         if i == 1 and first_name:
-            print(" " * left_padding + "8" + first_name.ljust(size * 2) + "8")
+            print(" " * left_padding + "|" + first_name.ljust(width - 3) + "|")
         elif i == 2 and tag:
-            print(" " * left_padding + "8" + tag.ljust(size * 2) + "8")
+            print(" " * left_padding + "|" + tag.ljust(width - 3) + "|")
         elif i == 3 and stopwatch:
-            print(" " * left_padding + "8" + stopwatch.ljust(size * 2) + "8")
+            print(" " * left_padding + "| " + stopwatch.ljust(width - 3) + " |")
         else:
-            print(" " * left_padding + "8" + " " * (size * 2) + "8")
+            print(" " * left_padding + "|" + " " * (width - 2) + "|")
     
-    print(" " * left_padding + "8" + "_" * (size * 2) + "8")
+    print(" " * left_padding + "+" + "-" * (width - 2) + "+")
 
 def clear_terminal():
-    if platform.system() == "Linux" or platform.system() == "Darwin":
-        os.system("clear")
-    elif platform.system() == "Windows":
-        os.system("cls")
+    os.system("cls" if os.name == "nt" else "clear")
 
-def handle_resize(signum, frame):
-    clear_terminal()
-    print_empty_square(size, first_name, tag, format_time(stopwatch))
 def update_stopwatch():
     global stopwatch
     while True:
         time.sleep(1)
         stopwatch += 1
-        handle_resize(None, None)
 
-if __name__ == "__main__":
-    size = 15
+def main(stdscr):
+    curses.curs_set(0)  # Hide cursor
+    stdscr.clear()
+    stdscr.refresh()
 
-    signal.signal(signal.SIGWINCH, handle_resize)
-
-    clear_terminal()
-    print_empty_square(size, first_name, tag, format_time(stopwatch))
+    size = (15, 10)  # Width, Height
     threading.Thread(target=update_stopwatch, daemon=True).start()
 
     while True:
-        try:
-            time.sleep(1)
-        except KeyboardInterrupt:
-            break
+        stdscr.clear()
+        print_empty_rectangle(*size, first_name, tag, format_time(stopwatch))
+        stdscr.refresh()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    curses.wrapper(main)
