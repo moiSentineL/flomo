248cee22f264ae5b77d6c89572d415ac95a6167b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-06-06 22:38:57 +0530

    formatting, partial refactoring and removing unnecessary stuff

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 import click
 import src.ui as ui
-# from src.logic import Flow
-
-# flow = Flow()
-
-# flow.initialize_flow()
-# print("Started flow!")
-
-# print("Waiting for input..")
-# input()
-
-# flow.break_flow()
-# print("Time elapsed: ", flow.time_elapsed, "s")
-# print("Time for break: ", flow.time_for_break, "s")
-
-# # Wait for flow.time_for_break
-# flow.restart_flow()
-# print("Restarted Flow!")
-
-# # repeat cycle until stop_flow
 
 
 @click.group()
@@ -31,10 +12,7 @@ def hello():
 @click.option('-n', '--name', default='Work',
               help='Session Name')
 def start(tag, name):
-    # click.echo("started")
-    # click.echo(f"tag: {tag}")
-    # click.echo(f"name: {name}")
-    ui.main(tag, name)
+    ui.main(f"#{tag}", name)
 
 
 if __name__ == '__main__':
