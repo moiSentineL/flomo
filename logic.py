c3bceef69a54e229178bf3ae50b10c6305ef89ab
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-06-06 13:36:59 +0530

    gotta implement the restart_flow function

--- logic.py
+++ logic.py
@@ -4,18 +4,15 @@ import time
 class Flow:
     def __init__(self) -> None:
         self.start_time = 0
+        self.time_elapsed = 0
         self.time_for_break = 0
 
     def initialize_flow(self):
         self.start_time = time.time()
-        print("Started flow!")
 
-    def start_flow(self):
+    def restart_flow(self):
         pass
 
     def break_flow(self):
-        time_elapsed = round(time.time() - self.start_time, 2)
-        print("Time elapsed: ", time_elapsed, "s")
-
-        time_for_break = round(time_elapsed/5, 2)
-        print("Time for break: ", time_for_break, "s")
+        self.time_elapsed = round(time.time() - self.start_time, 2)
+        self.time_for_break = round(self.time_elapsed/5, 2)
