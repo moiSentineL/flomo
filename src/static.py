750bef4bd3c5d366a786c5d7651cf47d2d505023
moiSentineL <nibir.patowary@gmail.com>
2024-06-06 16:43:35 +0530

    using size variable

--- src/static.py
+++ src/static.py
@@ -5,30 +5,32 @@ import threading
 
 first_name = " First Name: "
 tag = " Tag: "
-stopwatch = 0  
+stopwatch = 0
+
+size = 50
 
 def format_time(seconds):
     mins, secs = divmod(seconds, 60)
     return f"{mins:02}:{secs:02}"
 
 def print_empty_rectangle(width, height, first_name="", tag="", stopwatch=""):
-    left_padding = (width - 30) // 2
+    left_padding = (width - size) // 2
     top_padding = (height - 7) // 2
     
     print("\n" * top_padding, end="")
-    print(" " * left_padding + "+" + "-" * 28 + "+")
+    print(" " * left_padding + "+" + "----FLOMO" + "-"*(size-9) + "+")
     
-    for i in range(5):
+    for i in range(7):
         if i == 1 and first_name:
-            print(" " * left_padding + "|" + first_name.center(28) + "|")
+            print(" " * left_padding + "|" + first_name.center(size) + "|")
         elif i == 2 and tag:
-            print(" " * left_padding + "|" + tag.center(28) + "|")
+            print(" " * left_padding + "|" + tag.center(size) + "|")
         elif i == 3 and stopwatch:
-            print(" " * left_padding + "|" + stopwatch.center(28) + "|")
+            print(" " * left_padding + "|" + stopwatch.center(size) + "|")
         else:
-            print(" " * left_padding + "|" + " " * 28 + "|")
+            print(" " * left_padding + "|" + " " * size + "|")
     
-    print(" " * left_padding + "+" + "-" * 28 + "+")
+    print(" " * left_padding + "+" + "-" * size + "+")
 
 def clear_terminal():
     os.system("cls" if os.name == "nt" else "clear")
