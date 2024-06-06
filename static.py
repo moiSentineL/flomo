8646fd9713a0629615b9c70c28bb556ac40a73de
moiSentineL <nibir.patowary@gmail.com>
2024-06-06 16:18:31 +0530

    static.py working in windows?

--- static.py
+++ static.py
@@ -2,7 +2,6 @@ import shutil
 import os
 import time
 import threading
-import curses
 
 first_name = " First Name: "
 tag = " Tag: "
@@ -13,24 +12,23 @@ def format_time(seconds):
     return f"{mins:02}:{secs:02}"
 
 def print_empty_rectangle(width, height, first_name="", tag="", stopwatch=""):
-    terminal_size = shutil.get_terminal_size()
-    left_padding = (terminal_size.columns - width) // 2
-    top_padding = (terminal_size.lines - height) // 2
+    left_padding = (width - 30) // 2
+    top_padding = (height - 7) // 2
     
     print("\n" * top_padding, end="")
-    print(" " * left_padding + "+" + "-" * (width - 2) + "+")
+    print(" " * left_padding + "+" + "-" * 28 + "+")
     
-    for i in range(height - 2):
+    for i in range(5):
         if i == 1 and first_name:
-            print(" " * left_padding + "|" + first_name.ljust(width - 3) + "|")
+            print(" " * left_padding + "|" + first_name.center(28) + "|")
         elif i == 2 and tag:
-            print(" " * left_padding + "|" + tag.ljust(width - 3) + "|")
+            print(" " * left_padding + "|" + tag.center(28) + "|")
         elif i == 3 and stopwatch:
-            print(" " * left_padding + "| " + stopwatch.ljust(width - 3) + " |")
+            print(" " * left_padding + "|" + stopwatch.center(28) + "|")
         else:
-            print(" " * left_padding + "|" + " " * (width - 2) + "|")
+            print(" " * left_padding + "|" + " " * 28 + "|")
     
-    print(" " * left_padding + "+" + "-" * (width - 2) + "+")
+    print(" " * left_padding + "+" + "-" * 28 + "+")
 
 def clear_terminal():
     os.system("cls" if os.name == "nt" else "clear")
@@ -41,19 +39,18 @@ def update_stopwatch():
         time.sleep(1)
         stopwatch += 1
 
-def main(stdscr):
-    curses.curs_set(0)  # Hide cursor
-    stdscr.clear()
-    stdscr.refresh()
+def get_terminal_size():
+    columns, lines = shutil.get_terminal_size()
+    return columns, lines
 
-    size = (15, 10)  # Width, Height
+def main():
     threading.Thread(target=update_stopwatch, daemon=True).start()
 
     while True:
-        stdscr.clear()
-        print_empty_rectangle(*size, first_name, tag, format_time(stopwatch))
-        stdscr.refresh()
+        clear_terminal()
+        width, height = get_terminal_size()
+        print_empty_rectangle(width, height, first_name, tag, format_time(stopwatch))
         time.sleep(1)
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    main()
