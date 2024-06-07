a581e0c02baa0a3b5e1186230d12e516b17cc3ed
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-06-07 23:19:12 +0530

    solved skill issue & formatted

--- wip/bin.py
+++ wip/bin.py
@@ -8,6 +8,8 @@ import time
 
 
 console = Console()
+
+
 def rich_panel(status):
     global stopwatch
     stopwatch = 0
@@ -15,26 +17,23 @@ def rich_panel(status):
     # status = int(input("Enter 1 for WORKING and 2 for BREAK: ")) #This is the main val to chagne break time and working also the color (Degub- Remove this)
     # < redundant, passed on as parameter in main funtion
 
-
-    if status == 1 :
+    if status == 1:
         text = "WORKING"
         color_plot = "bold blue"
     elif status == 2:
         text = "BREAK"
         color_plot = "bold red"
-        
-    content = "Content of the panel, to be done later with formatting"
-    title = "Flomo - " + text 
 
-    
+    content = "Content of the panel, to be done later with formatting"
+    title = "Flomo - " + text
 
     def format_time(seconds):
         hours, remainder = divmod(seconds, 3600)
         mins, secs = divmod(remainder, 60)
         return f"{hours:02}:{mins:02}:{secs:02}"
 
-    def update_stopwatch(): # for now redudant ...
-        
+    def update_stopwatch():  # for now redudant ...
+
         global stopwatch
         stopwatch = 0
 
@@ -43,26 +42,21 @@ def rich_panel(status):
             # if not stop_timer:
             stopwatch += 1
 
-
     def generate_panel() -> Panel:
 
         global stopwatch
-        content = Text(format_time(stopwatch)) 
+        content = Text(format_time(stopwatch))
         stopwatch += 1
         panel = Panel(content, expand=False, title=title,
                       border_style=color_plot, title_align="left")
         # stopwatch += 1
         return panel
 
-
-
-
-    with Live(generate_panel(), refresh_per_second=4):
+    with Live(generate_panel(), refresh_per_second=4) as thislivethingie:
         # stop_timer = False -> can be used as a failsafe?
         for n in range(5):
             time.sleep(0.4)
-            Live.update(generate_panel())
-
+            Live.update(thislivethingie, generate_panel())
 
 
 if __name__ == "__main__":
