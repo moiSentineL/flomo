e613070889b952554d5218525bf6454c8370ecad
AnubhavSC <anubhavnath60@gmail.com>
2024-06-07 15:58:31 +0530

    empty commit

--- wip/bin.py
+++ wip/bin.py
@@ -3,34 +3,32 @@ from rich.panel import Panel
 # from rich.table import Table
 from rich.align import Align
 
-
-console = Console()
-
 # table = Table(title="test bro")
 
 # table.add_column("Name: tag:", justify="right", no_wrap=True)
 # table.add_row("hello")
 # table.add_row("Bru")
 
-# console.print(table)
-status = int(input("Enter 1 for WORKING and 2 for BREAK: ")) #This is the main val to chagne break time and working also the color (Degub- Remove this)
+console = Console()
 
+status = int(input("Enter 1 for WORKING and 2 for BREAK: "))  # This is the main value to change break time and working also the color (Debug - Remove this)
 
-
-if status == 1 :
+if status == 1:
     text = "WORKING"
     color_plot = "bold blue"
 elif status == 2:
     text = "BREAK"
     color_plot = "bold red"
-    
-content = "Content of the panel, to be done later with formatting"
-title = "Flomo - " + text 
 
+content = "Content of the panel, to be done later with formatting"
+title = "Flomo - " + text
+
+#  height and width of the box
+height = 10
+width = 50
 
 panel = Panel(content, expand=False, title=title,
-              border_style=color_plot, title_align="left")
-
+              border_style=color_plot, title_align="left", height=height, width=width)
 
 center_panel = Align.center(panel)
 
