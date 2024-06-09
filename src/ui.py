e679729a4ac3aa8f74afd1ee8a29d4ba4c1fce02
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-06-09 20:52:00 +0530

    oop is always refactoring!

--- src/ui.py
+++ src/ui.py
@@ -5,52 +5,33 @@ from rich.text import Text
 import time
 
 
-console = Console()
+class UI:
+    def __init__(self, status: int):
+        self.console = Console()
+        self.stopwatch = 0
 
+        self.title = "Flomo - " + ("WORKING" if status == 1 else "BREAK")
+        self.border_style = "bold blue" if status == 1 else "bold red"
 
-def rich_panel(status):
-    global stopwatch
-    stopwatch = 0
-
-    if status == 1:
-        text = "WORKING"
-        color_plot = "bold blue"
-    elif status == 2:
-        text = "BREAK"
-        color_plot = "bold red"
-
-    title = "Flomo - " + text
-
-    def format_time(seconds):
+    def format_time(self, seconds: int):
         hours, remainder = divmod(seconds, 3600)
         mins, secs = divmod(remainder, 60)
         return f"{hours:02}:{mins:02}:{secs:02}"
 
-    # def update_stopwatch():  # for now redudant ...
-    #     global stopwatch
-    #     stopwatch = 0
-
-    #     while True:
-    #         time.sleep(1)
-    #         # if not stop_timer:
-    #         stopwatch += 1
-
-    def generate_panel() -> Panel:
-        global stopwatch
-        content = Text(format_time(stopwatch))
-        stopwatch += 1
-        panel = Panel(content, expand=False, title=title,
-                      border_style=color_plot, title_align="left")
-        # stopwatch += 1
-        # return Align.center(panel)
+    def generate_panel(self):
+        content = Text(self.format_time(self.stopwatch))
+        panel = Panel(content, expand=False, title=self.title,
+                      border_style=self.border_style, title_align="left")
         return panel
 
-    with Live(generate_panel(), refresh_per_second=4) as _live:
-        # stop_timer = False -> can be used as a failsafe?
-        while True:
-            time.sleep(1)
-            Live.update(_live, generate_panel())
+    def show_live_panel(self):
+        with Live(self.generate_panel(), refresh_per_second=4) as _live:
+            while True:
+                time.sleep(1)
+                self.stopwatch += 1
+                Live.update(_live, self.generate_panel())
 
 
 if __name__ == "__main__":
-    rich_panel(1)
+    ui = UI(1)
+    ui.show_live_panel()
