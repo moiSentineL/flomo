bb845eb54da85bda119e51422eea3a588a14ff2b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-07-14 20:12:07 +0530

    fixing some wierd bug!

--- flomo/tracker.py
+++ flomo/tracker.py
@@ -16,14 +16,14 @@ class Tracker:
 
     def create_table(self):
         self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS sessions (id FLOAT PRIMARY KEY, date_time, tag TEXT, name TEXT, total_time TEXT)"
+            "CREATE TABLE IF NOT EXISTS sessions (id FLOAT PRIMARY KEY, date_time TEXT, tag TEXT, name TEXT, total_time TEXT)"
         )
         self.conn.commit()
 
     def create_session(
         self, tag: str, name: str, start_time: datetime.datetime
     ) -> float:
-        session_id = start_time.timestamp()
+        session_id = start_time.timestamp() % 1000000
         self.cursor.execute(
             "INSERT INTO sessions (id, date_time, tag, name) VALUES (?, ?, ?, ?)",
             (session_id, start_time.strftime("%Y-%m-%d %H:%M:%S"), tag, name),
@@ -32,7 +32,10 @@ class Tracker:
         return session_id
 
     def update_session(self, session_id: float, end_time: datetime.datetime):
-        total_time = end_time - datetime.datetime.fromtimestamp(session_id)
+        date_time = self.get_session(session_id)[1]
+        total_time = end_time - datetime.datetime.strptime(
+            date_time, "%Y-%m-%d %H:%M:%S"
+        )
         self.cursor.execute(
             "UPDATE sessions SET total_time = ? WHERE id = ?",
             (
