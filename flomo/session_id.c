6e6992a30b59d0fe2098540baea7a00f3713535b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-07-30 08:51:46 +0530

    formatted the c code

--- flomo/session_id.c
+++ flomo/session_id.c
@@ -5,17 +5,20 @@
 const char ALPHANUMERIC[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const int BASE = 62;
 
-char* encode_timestamp(unsigned long timestamp) {
-    char *encoded = (char*)malloc(12);
+char *encode_timestamp(unsigned long timestamp)
+{
+    char *encoded = (char *)malloc(12);
     int index = 0;
 
-    do {
+    do
+    {
         int rem = timestamp % BASE;
         encoded[index++] = ALPHANUMERIC[rem];
         timestamp /= BASE;
     } while (timestamp > 0);
 
-    for (int i = 0; i < index/2; i++) {
+    for (int i = 0; i < index / 2; i++)
+    {
         char temp = encoded[i];
         encoded[i] = encoded[index - i - 1];
         encoded[index - i - 1] = temp;
@@ -25,13 +28,16 @@ char* encode_timestamp(unsigned long timestamp) {
     return encoded;
 }
 
-unsigned long decode_timestamp(const char* encoded) {
+unsigned long decode_timestamp(const char *encoded)
+{
     unsigned long timestamp = 0;
     size_t length = strlen(encoded);
 
-    for (size_t i = 0; i < length; i++) {
+    for (size_t i = 0; i < length; i++)
+    {
         char *ptr = strchr(ALPHANUMERIC, encoded[i]);
-        if (ptr) {
+        if (ptr)
+        {
             int index = ptr - ALPHANUMERIC;
             timestamp = timestamp * BASE + index;
         }
